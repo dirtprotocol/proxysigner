06ad61e1a523560297fdf35c57987d72dc6e9b9d
Mark Erdmann <mark.erdmann@gmail.com>
2019-07-11 19:23:26 -0500

    v0 of the proxy signer

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# proxysigner
+## Requirements
+
+1. A PROXY_SECRET_KEY env variable that contains your private key.
+2. A WEB3_PROVIDER_URL env variable (e.g. an Infura URL).
+
+## Run
+
+1. `yarn install`
+2. `node index.js`
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const fetch = require('node-fetch')
+
+const EcSign = require('./lib/ec_sign')
+
+app.get('/', async (req, res) => {
+  const data = await fetchData(req.query)
+  console.log(data)
+  const signedData = EcSign.signAndPackPrice(
+    data.price, 
+    data.timestamp,
+    data.market
+  )
+  res.send(signedData)
+})
+
+// This is specific to the source that is being proxied. Replace this with
+// logic appropriate to your source.
+async function fetchData(params) {
+  const pair = params.pair
+  const url = `https://api.kraken.com/0/public/Ticker?pair=${pair}`
+  const ret = await fetch(url)
+  const json = await ret.json()
+  return {
+    market: pair,
+    price: json.result[pair].c[0],
+    timestamp: new Date().getTime()
+  }
+}
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
