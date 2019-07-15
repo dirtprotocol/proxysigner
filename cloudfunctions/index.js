4f2c10aaa8c99b7be20d0269791bcae55011e57b
Mark Erdmann <mark.erdmann@gmail.com>
2019-07-15 16:58:23 -0500

    Add Cloud Functions version of index.js

---
+++ cloudfunctions/index.js
@@ -0,0 +1,47 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+
+const Web3 = require('web3')
+const fetch = require('node-fetch')
+
+const provider = new Web3.providers.HttpProvider(
+  process.env.WEB3_PROVIDER_URL,
+  'http://localhost:8545'
+)
+const web3 = new Web3(provider)
+
+function signAndPackPrice(price, timestamp, market) {
+  let dataHash = web3.utils.soliditySha3(
+    {t: 'uint256', v: price},
+    {t: 'uint256', v: timestamp},
+    {t: 'bytes32', v: web3.utils.toHex(market)
+  })
+
+  return web3.eth.accounts.sign(dataHash, process.env.PROXY_SECRET_KEY)
+}
+
+async function fetchData(params) {
+  const pair = params.pair
+  const url = `https://api.kraken.com/0/public/Ticker?pair=${pair}`
+  const ret = await fetch(url)
+  const json = await ret.json()
+  return {
+    market: pair,
+    price: json.result[pair].c[0],
+    timestamp: new Date().getTime()
+  }
+}
+
+exports.main = async (req, res) => {
+  const data = await fetchData(req.query)
+  const signedData = signAndPackPrice(
+    data.price, 
+    data.timestamp,
+    data.market
+  )
+  res.send(signedData)
+};
